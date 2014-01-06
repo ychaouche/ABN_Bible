1b9c6493164f7d19100e768dd9713950707c4633
Kumaran <atv.kumar@gmail.com>
2014-01-06 12:51:17 +0530

    Updated Language with translation

--- Language.py
+++ Language.py
@@ -2,10 +2,11 @@
 
 
 class TranslationTable(object):
-    def __init__(self, Chr=None, Uni=None, t1=None):
+    def __init__(self, Chr=None, Uni=None, t1=None, pos=None):
         self.chr_name = Chr
         self.uni_value = Uni
         self.t1_value = t1
+        self.t1_pos = pos
 
     def __str__(self):
         return self.uni_value.encode("UTF-8")
@@ -15,21 +16,21 @@ class TranslationTable(object):
 
 
 class Tamil(object):  # Unicode Tamil
-    om = TranslationTable('OM', u'\u0BD0')
-    dot = TranslationTable('pulli', u'\u0BCD', u'\xf6')
-    leg = TranslationTable('VOWEL SIGN AA', u'\u0BBE', u'V')
-    smhorn = TranslationTable('VOWEL SIGN I', u'\u0BBF', u'\xe5')
-    bghorn = TranslationTable('VOWEL SIGN II', u'\u0BC0', u'\xe7')
-    smu = TranslationTable('VOWEL SIGN U', u'\u0BC1', u'\xe9')
-    bguu = TranslationTable('VOWEL SIGN UU', u'\u0BC2', u'\xe8')
-    sme = TranslationTable('VOWEL SIGN E', u'\u0BC6', u'\xff')
-    bgee = TranslationTable('VOWEL SIGN EE', u'\u0BC7', u'\xba')
-    aii = TranslationTable('VOWEL SIGN AI', u'\u0BC8', u'\xc1')
+    om = TranslationTable('OM', u'\u0BD0')  # No Anu
+    dot = TranslationTable('pulli', u'\u0BCD', u'\xf6')  # No Anu
+    leg = TranslationTable('VOWEL SIGN AA', u'\u0BBE', u'V', 1)
+    smhorn = TranslationTable('VOWEL SIGN I', u'\u0BBF', u'\xe5')  # No Anu
+    bghorn = TranslationTable('VOWEL SIGN II', u'\u0BC0', u'\xe7')  # No Anu
+    smu = TranslationTable('VOWEL SIGN U', u'\u0BC1', u'\xe9')  # No Anu
+    bguu = TranslationTable('VOWEL SIGN UU', u'\u0BC2', u'\xe8')  # No Anu
+    sme = TranslationTable('VOWEL SIGN E', u'\u0BC6', u'\xff', 0)
+    bgee = TranslationTable('VOWEL SIGN EE', u'\u0BC7', u'\xba', 0)
+    aii = TranslationTable('VOWEL SIGN AI', u'\u0BC8', u'\xc1', 0)
     smo = TranslationTable('VOWEL SIGN O', u'\u0BCA')
     bgoo = TranslationTable('VOWEL SIGN OO', u'\u0BCB')
     smau = TranslationTable('VOWEL SIGN AU', u'\u0BCC')
     llaau = TranslationTable('AU LENGTH MARK', u'\u0BD7')
-    signs = [dot, leg, smhorn, bghorn, smu, bguu, sme, bgee, aii, smo, bgoo,
+    signs = [dot, smhorn, bghorn, smu, bguu, sme, bgee, aii, smo, bgoo,
              smau, llaau]
     a = TranslationTable('A', u'\u0B85', u'\u2202')
     aa = TranslationTable('AA', u'\u0B86', u'g')
@@ -70,8 +71,89 @@ class Tamil(object):  # Unicode Tamil
     ssa = TranslationTable('SSA', u'\u0BB7', u'\u2260')
     sa = TranslationTable('SA', u'\u0BB8', u'v')
     ha = TranslationTable('HA', u'\u0BB9', u'\xab')
+    fancy = [sha, ssa, sa, ha]
 
 tm = Tamil()
-#for i in tm.indep_vowels: print i
-for i in tm.cons: print i
-#for i in tm.signs: print i
+trans = {}
+anu_consDot = unicode("¬∫áÎâ‚ı›Õ[©DFÏu_^µÀ", "UTF-8")
+anu_smhorn = unicode("˛EÉË]WM∏tlˆ§oÑas", "UTF-8")
+anu_bghorn = unicode("ˇÊœßy¿Ã¨*XZS‹C—T", "UTF-8")
+anu_smu = unicode("zà|ÓmO–Ax•ÚÆK”ø°", "UTF-8")
+anu_bguu = unicode("ÌÛ˘I#±˚ØJR‘GŸjÒ∆", "UTF-8")
+tm_smhorn = []
+tm_bghorn = []
+tm_smu = []
+tm_bguu = []
+tm_sme = []
+tm_bgee = []
+tm_ai = []
+tm_smo = []
+tm_bgoo = []
+tm_smau = []
+
+
+for x in tm.indep_vowels+tm.cons+tm.fancy:
+    trans[x.uni_value] = x.t1_value
+
+trans[tm.leg.uni_value] = tm.leg.t1_value
+
+for i, x in enumerate(tm.cons):
+    tm_smhorn.append(x.uni_value+tm.smhorn.uni_value)
+    tm_bghorn.append(x.uni_value+tm.bghorn.uni_value)
+    tm_smu.append(x.uni_value+tm.smu.uni_value)
+    tm_bguu.append(x.uni_value+tm.bguu.uni_value)
+    tm_sme.append(x.uni_value+tm.sme.uni_value)
+    tm_bgee.append(x.uni_value+tm.bgee.uni_value)
+    tm_ai.append(x.uni_value+tm.aii.uni_value)
+    tm_smo.append(x.uni_value+tm.smo.uni_value)
+    tm_bgoo.append(x.uni_value+tm.bgoo.uni_value)
+    tm_smau.append(x.uni_value+tm.smau.uni_value)
+    cons_dot = x.uni_value+tm.dot.uni_value
+    trans[cons_dot] = anu_consDot[i]
+
+#TODO check and update if AnuFonts support the characters
+tm_smhorn.pop(1)  # ஙி not in Anu
+tm_smhorn.pop(2)  # ஜி not in Anu
+tm_smhorn.pop(2)  # ஞி not in Anu
+
+tm_bghorn.pop(1)  # ஙீ not in Anu
+tm_bghorn.pop(2)  # ஜீ not in Anu
+tm_bghorn.pop(2)  # ஞீ not in Anu
+
+tm_smu.pop(1)  # ஙு not in Anu
+tm_smu.pop(2)  # ஜு not in Anu
+tm_smu.pop(2)  # ஞு not in Anu
+
+tm_bguu.pop(1)  # ஙூ not in Anu
+tm_bguu.pop(2)  # ஜூ not in Anu
+tm_bguu.pop(2)  # ஞூ not in Anu
+
+for i, x in enumerate(tm_smhorn):
+    trans[x] = anu_smhorn[i]
+
+for i, x in enumerate(tm_bghorn):
+    trans[x] = anu_bghorn[i]
+
+for i, x in enumerate(tm_smu):
+    trans[x] = anu_smu[i]
+
+for i, x in enumerate(tm_bguu):
+    trans[x] = anu_bguu[i]
+
+for i, x in enumerate(tm_sme):
+    trans[x] = tm.sme.t1_value+tm.cons[i].t1_value
+
+for i, x in enumerate(tm_bgee):
+    trans[x] = tm.bgee.t1_value+tm.cons[i].t1_value
+
+for i, x in enumerate(tm_ai):
+    trans[x] = tm.aii.t1_value+tm.cons[i].t1_value
+
+for i, x in enumerate(tm_smo):
+    trans[x] = tm.sme.t1_value+tm.cons[i].t1_value+tm.leg.t1_value
+
+for i, x in enumerate(tm_bgoo):
+    trans[x] = tm.bgee.t1_value+tm.cons[i].t1_value+tm.leg.t1_value
+
+for i, x in enumerate(tm_smau):
+    trans[x] = tm.sme.t1_value+tm.cons[i].t1_value+tm.lla.t1_value
